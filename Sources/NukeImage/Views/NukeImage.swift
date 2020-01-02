3048b3b903ff853b3b1acc28a85a091e4ab52c4f
Stefano Bertagno <stefano.bertagno@gmail.com>
2020-01-02 21:43:56 +0100

    Improve multiple requests support

--- Sources/NukeImage/Views/NukeImage.swift
+++ Sources/NukeImage/Views/NukeImage.swift
@@ -9,9 +9,11 @@ import Nuke
 import SwiftUI
 
 /// A basic `NukeImageView`, with `Image` placeholder support.
-public struct NukeImage: NukeImageView {
+public struct NukeImage<Request: NukeRequestable>: NukeImageView {
     /// The request image.
-    @Binding var request: NukeRequestable
+    @Binding var request: Request
+    /// The previous request.
+    @State private var previousRequest: Request?
     /// The image.
     @State private var image: Nuke.Image? = nil
     
@@ -24,12 +26,12 @@ public struct NukeImage: NukeImageView {
     
     // MARK: Init
     /// Init with request.
-    public init(_ request: Binding<NukeRequestable>) {
+    public init(_ request: Binding<Request>) {
         self._request = request
     }
     /// Init with request.
-    public init<Request>(_ request: Request) where Request: NukeRequestable {
-        self._request = .constant(request.imageRequest)
+    public init(_ request: Request) {
+        self._request = .constant(request)
     }
         
     // MARK: Lifecycle
@@ -44,12 +46,18 @@ public struct NukeImage: NukeImageView {
     }
     /// The actual body.
     public var body: some View {
-        imageBody.onAppear { [imageRequest = request.imageRequest] in
-            // load pipeline.
-            self.image = nil
-            Nuke.ImagePipeline.shared.loadImage(with: imageRequest) {
-                self.image = try? $0.get().image
-            }
+        // fetch new image when necessary.
+        if previousRequest?.imageRequest.urlRequest.url != request.imageRequest.urlRequest.url { fetch() }
+        return imageBody
+    }
+    
+    // MARK: Fetch
+    func fetch() {
+        // load pipeline.
+        self.previousRequest = request
+        self.image = nil
+        Nuke.ImagePipeline.shared.loadImage(with: request.imageRequest) {
+            self.image = try? $0.get().image
         }
     }
 }
@@ -70,6 +78,6 @@ public extension NukeImage {
     }
     /// Set `placeholder`.
     func placeholder<V>(_ placeholder: V) -> some NukeImageView where V: View {
-        PlaceholderNukeImage<V>($request).placeholder(placeholder)
+        PlaceholderNukeImage<V, Request>($request).placeholder(placeholder)
     }
 }
